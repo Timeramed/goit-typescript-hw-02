e0c9fc2ca1c8f20215e721e7768a8c5ed26dda88
Artem Hol <166126006+Timeramed@users.noreply.github.com>
2024-04-10 05:17:52 +0200

    Update 3.ts

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -5,6 +5,6 @@
 type AnyTipe = {
   type: any;
 };
-function merge<T, U>(objA: T, objB: U): T & U {
-  return Object.assign({}, objA, objB);
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
+  return Object.assign(objA, objB);
 }
